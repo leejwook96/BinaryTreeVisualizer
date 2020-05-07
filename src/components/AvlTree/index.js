3de9f31d7548e70d169e1e8c0ee486676d0060de
Jae Lee <leejwook0429@gmail.com>
2020-05-07 04:41:27 -0500

    Add title; reset button; automatic rebalance checkbox for avl

--- src/components/AvlTree/index.js
+++ src/components/AvlTree/index.js
@@ -2,8 +2,17 @@ import React from "react";
 import "../../App.scss";
 import BinaryTreeRender from "../BinaryTreeRender";
 import { FormControl, InputGroup, Button } from "react-bootstrap";
+import Alert from "@material-ui/lab/Alert";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Slider, Typography, Grid } from "@material-ui/core";
+import {
+  Slider,
+  Typography,
+  Grid,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  Snackbar,
+} from "@material-ui/core";
 
 import AvlTreeClass from "../../objects/AvlTree";
 
@@ -12,11 +21,15 @@ class AvlTree extends React.Component {
     super(props);
     this.textInput = React.createRef();
     this.transitionDurationValue = 500;
-    this.nextId = 4;
     this.avl = new AvlTreeClass();
+    this.SBSeverity = "";
+    this.SBMsg = "";
     this.state = {
       data: [{ name: "NULL" }],
       transitionDuration: 500,
+      rebalanceCheckbox: true,
+      balanceTreeBtnDisabled: true,
+      snackBarOpen: false,
     };
   }
 
@@ -27,7 +40,9 @@ class AvlTree extends React.Component {
     }
     this.avl.insertValue(value);
     this.textInput.current.value = "";
-    this.avl.balance();
+    if (this.state.rebalanceCheckbox) {
+      this.avl.balance();
+    }
     this.setState({
       data: this.avl.convertToD3Tree(),
       transitionDuration: this.transitionDurationValue,
@@ -44,86 +59,157 @@ class AvlTree extends React.Component {
     this.transitionDurationValue = newValue;
   };
 
+  handleResetButtonClick = () => {
+    this.setState({ data: [{ name: "NULL" }] });
+    this.avl = new AvlTreeClass();
+  };
+
+  renderSnackBar = () => {
+    let closeSnackbar = () => {
+      this.setState({ snackBarOpen: false });
+    };
+    return (
+      <Snackbar
+        open={this.state.snackBarOpen}
+        autoHideDuration={1000}
+        onClose={closeSnackbar}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert onClose={closeSnackbar} severity={this.SBSeverity}>
+          {this.SBMsg}
+        </Alert>
+      </Snackbar>
+    );
+  };
+
+  balanceIfNotBalanced = () => {
+    if (this.avl.root === null) {
+      // TODO Error Alert. Tree is null
+      this.SBMsg = "Tree is empty!";
+      this.SBSeverity = "error";
+      this.setState({ snackBarOpen: true });
+      return;
+    }
+    if (this.avl.isBalanced()) {
+      // TODO Info Alert. Tree is already balanced
+      this.SBMsg = "Tree is already balanced";
+      this.SBSeverity = "info";
+      this.setState({ snackBarOpen: true });
+      return;
+    } else {
+      this.avl.balance();
+      this.setState({
+        data: this.avl.convertToD3Tree(),
+        transitionDuration: this.transitionDurationValue,
+      });
+    }
+  };
+
+  renderAddNodeInputGroup = () => {
+    return (
+      <InputGroup className="mb-3" onKeyPress={this.handleKeyPress}>
+        <InputGroup.Prepend>
+          <InputGroup.Text>Add more numbers</InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl
+          ref={this.textInput}
+          type="text"
+          aria-label=""
+          aria-describedby="basic-addon2"
+        />
+        <InputGroup.Append>
+          <Button variant="primary" onClick={this.addNumber} type="submit">
+            Add
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
+    );
+  };
+
+  renderRenderSliderBar = () => {
+    return (
+      <>
+        <Typography id="continuous-slider" gutterBottom>
+          <b>Render Speed</b>
+        </Typography>
+
+        <Grid container spacing={2}>
+          <Grid item>Faster</Grid>
+          <Grid item xs>
+            <Slider
+              defaultValue={this.transitionDurationValue}
+              step={100}
+              min={0}
+              max={5000}
+              onChange={this.handleValueChange}
+              aria-labelledby="continuous-slider"
+            />
+          </Grid>
+          <Grid item>Slower</Grid>
+        </Grid>
+      </>
+    );
+  };
+
+  renderAutomaticBalanceCheckbar = () => {
+    return (
+      <FormGroup col>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={this.state.rebalanceCheckbox}
+              onChange={(e) => {
+                this.setState({
+                  balanceTreeBtnDisabled: e.target.checked,
+                  rebalanceCheckbox: e.target.checked,
+                });
+              }}
+            />
+          }
+          label="Rebalance Automatically"
+        />
+        <Button
+          variant="primary"
+          onClick={this.balanceIfNotBalanced}
+          disabled={this.state.balanceTreeBtnDisabled}
+        >
+          Balance Tree
+        </Button>
+      </FormGroup>
+    );
+  };
+
+  renderResetBtn = () => {
+    return (
+      <Button
+        variant="danger"
+        className="float-right"
+        onClick={this.handleResetButtonClick}
+      >
+        Reset
+      </Button>
+    );
+  };
+
   render() {
-    // let avl = new AvlTreeClass();
-    // avl.root = new Node(20);
-    // avl.root.left = new Node(10);
-    // avl.root.right = new Node(40);
-    // avl.root.right.left = new Node(25);
-    // avl.root.right.left.right = new Node(30);
-    // avl.root.right = avl.root.right.LRRotation();
-    // let data = avl.convertToD3Tree();
-
-    // let avl = new AvlTreeClass();
-    // avl.root = new Node(20);
-    // avl.root.right = new Node(30);
-    // avl.root.right.right = new Node(40);
-    // avl.root = avl.root.RRRotation();
-    // let data = avl.convertToD3Tree();
-
-    // let avl = new AvlTreeClass();
-    // avl.root = new Node(20);
-    // avl.root.addLeft(new Node(10));
-    // avl.root.addRight(new Node(30));
-    // avl.root.right.addRight(new Node(40));
-    // avl.root.right.addLeft(new Node(25));
-    // avl.root.right.left.addLeft(new Node(22));
-
-    // avl.root = avl.root.RLRotation();
-    // let data = avl.convertToD3Tree();
-
-    // avl.root.getAllHeight();
-    // avl.printHeight();
     return (
       <div className="main-container">
+        <br />
+        <h1>Binary Search Tree Visualizer</h1>
         <div className="tree-container">
-          <div className="bst-add-form">
-            <InputGroup className="mb-3" onKeyPress={this.handleKeyPress}>
-              <InputGroup.Prepend>
-                <InputGroup.Text>Add more numbers</InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl
-                ref={this.textInput}
-                type="text"
-                aria-label=""
-                aria-describedby="basic-addon2"
-              />
-              <InputGroup.Append>
-                <Button
-                  variant="primary"
-                  onClick={this.addNumber}
-                  type="submit"
-                >
-                  Add
-                </Button>
-              </InputGroup.Append>
-            </InputGroup>
-
-            <Typography id="continuous-slider" gutterBottom>
-              <b>Render Speed</b>
-            </Typography>
-
-            <Grid container spacing={2}>
-              <Grid item>Faster</Grid>
-              <Grid item xs>
-                <Slider
-                  defaultValue={this.transitionDurationValue}
-                  step={100}
-                  min={0}
-                  max={5000}
-                  onChange={this.handleValueChange}
-                  aria-labelledby="continuous-slider"
-                />
-              </Grid>
-              <Grid item>Slower</Grid>
-            </Grid>
+          <div className="bst-add-form float-left">
+            {this.renderAddNodeInputGroup()}
+            {this.renderRenderSliderBar()}
+            {this.renderAutomaticBalanceCheckbar()}
           </div>
+          {this.renderResetBtn()}
           <BinaryTreeRender
             data={this.state.data}
-            // data={data}
             transitionDurationValue={this.state.transitionDuration}
           />
         </div>
+
+        {this.renderSnackBar()}
       </div>
     );
   }
--- src/components/BinarySearchTree/index.js
+++ src/components/BinarySearchTree/index.js
@@ -12,7 +12,7 @@ class BinarySearchTree extends React.Component {
     super(props);
     this.textInput = React.createRef();
     this.transitionDurationValue = 500;
-    this.root = new BinarySearchTreeClass();
+    this.bst = new BinarySearchTreeClass();
     this.state = {
       data: [{ name: "NULL" }],
       transitionDuration: 500,
@@ -24,10 +24,10 @@ class BinarySearchTree extends React.Component {
     if (isNaN(value)) {
       return;
     }
-    this.root.insertValue(value);
+    this.bst.insertValue(value);
     this.textInput.current.value = "";
     this.setState({
-      data: this.root.convertToD3Tree(),
+      data: this.bst.convertToD3Tree(),
       transitionDuration: this.transitionDurationValue,
     });
   };
@@ -42,56 +42,84 @@ class BinarySearchTree extends React.Component {
     this.transitionDurationValue = newValue;
   };
 
+  handleResetButtonClick = () => {
+    this.setState({ data: [{ name: "NULL" }] });
+    this.bst = new BinarySearchTreeClass();
+  };
+
+  renderAddNodeInputGroup = () => {
+    return (
+      <InputGroup className="mb-3" onKeyPress={this.handleKeyPress}>
+        <InputGroup.Prepend>
+          <InputGroup.Text>Add more numbers</InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl
+          ref={this.textInput}
+          type="text"
+          aria-label=""
+          aria-describedby="basic-addon2"
+        />
+        <InputGroup.Append>
+          <Button variant="primary" onClick={this.addNumber} type="submit">
+            Add
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
+    );
+  };
+
+  renderRenderSliderBar = () => {
+    return (
+      <>
+        <Typography id="continuous-slider" gutterBottom>
+          <b>Render Speed</b>
+        </Typography>
+
+        <Grid container spacing={2}>
+          <Grid item>Faster</Grid>
+          <Grid item xs>
+            <Slider
+              defaultValue={this.transitionDurationValue}
+              step={100}
+              min={0}
+              max={5000}
+              onChange={this.handleValueChange}
+              aria-labelledby="continuous-slider"
+            />
+          </Grid>
+          <Grid item>Slower</Grid>
+        </Grid>
+      </>
+    );
+  };
+
+  renderResetBtn = () => {
+    return (
+      <Button
+        variant="danger"
+        className="float-right"
+        onClick={this.handleResetButtonClick}
+      >
+        Reset
+      </Button>
+    );
+  };
+
   render() {
     return (
       <div className="main-container">
+        <br />
+        <h1>AVL Tree Visualizer</h1>
         <div className="tree-container">
-          <div className="bst-add-form">
-            <InputGroup className="mb-3" onKeyPress={this.handleKeyPress}>
-              <InputGroup.Prepend>
-                <InputGroup.Text>Add more numbers</InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl
-                ref={this.textInput}
-                type="text"
-                aria-label=""
-                aria-describedby="basic-addon2"
-              />
-              <InputGroup.Append>
-                <Button
-                  variant="primary"
-                  onClick={this.addNumber}
-                  type="submit"
-                >
-                  Add
-                </Button>
-              </InputGroup.Append>
-            </InputGroup>
-
-            <Typography id="continuous-slider" gutterBottom>
-              <b>Render Speed</b>
-            </Typography>
-
-            <Grid container spacing={2}>
-              <Grid item>Faster</Grid>
-              <Grid item xs>
-                <Slider
-                  defaultValue={this.transitionDurationValue}
-                  step={100}
-                  min={0}
-                  max={5000}
-                  onChange={this.handleValueChange}
-                  aria-labelledby="continuous-slider"
-                />
-              </Grid>
-              <Grid item>Slower</Grid>
-            </Grid>
+          <div className="bst-add-form float-left">
+            {this.renderAddNodeInputGroup()}
+            {this.renderRenderSliderBar()}
           </div>
+          {this.renderResetBtn()}
           <BinaryTreeRender
             data={this.state.data}
             transitionDurationValue={this.state.transitionDuration}
           />
-          ;
         </div>
       </div>
     );
