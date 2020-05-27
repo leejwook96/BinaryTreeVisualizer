3727d6a59b1cd9b1ff5ebfbdfab8c69a99380407
Hayne Kim <khyein8154@gmail.com>
2020-05-27 15:00:12 -0500

    new href added to navbar for homepage

--- src/components/BTNavBar/index.js
+++ src/components/BTNavBar/index.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const BTNavBar = (props) => {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand>Binary Tree Visualizer</Navbar.Brand>
+      <Navbar.Brand href="/">Binary Tree Visualizer</Navbar.Brand>
       <Nav className="justify-content-center">
         <NavItem>
           <Nav.Link href="/bt">Binary Tree (BT) </Nav.Link>
